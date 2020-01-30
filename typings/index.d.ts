4bd779af807e55fb09914182df830714ba0a2664
John Goff <john.g@hiredhippo.ai>
2020-01-30 16:02:05 -0500

    update types to handle v3

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,1 @@
-export interface _VueStripeCheckout {
-  install: (Vue: any, options?: any) => void
-}
-export declare const VueStripeCheckout: _VueStripeCheckout;
-export default VueStripeCheckout;
+export interface VueStripeCheckout { StripeCheckout: Vue, StripeElements: Vue }
