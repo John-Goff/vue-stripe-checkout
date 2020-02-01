d76499a9cf83d9e37be2054ad690153480bf6c37
John Goff <john.g@hiredhippo.ai>
2020-02-01 00:44:22 -0500

    default export

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,2 @@
 import Vue from 'vue';
-export interface VueStripeCheckout { StripeCheckout: Vue, StripeElements: Vue }
+export default interface VueStripeCheckout { StripeCheckout: Vue, StripeElements: Vue }
